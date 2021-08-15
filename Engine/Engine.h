523e5b49f5a08fbb21a2aa65615ebad35fb22163
dmytrobesedin <dbesedin7@gmail.com>
2021-08-16 01:09:38 +0300

    First commit

---
+++ Engine/Engine.h
@@ -0,0 +1,18 @@
+//
+//  Engine.h
+//  Engine
+//
+//  Created by Дмитрий Беседин on 15.06.2021.
+//  Copyright © 2021 Dmytro Besedin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Engine.
+FOUNDATION_EXPORT double EngineVersionNumber;
+
+//! Project version string for Engine.
+FOUNDATION_EXPORT const unsigned char EngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Engine/PublicHeader.h>
+
---
+++ Engine/Engine.swift
@@ -0,0 +1,24 @@
+//
+//  Engine.swift
+//  Engine
+//
+//  Created by Дмитрий Беседин on 15.06.2021.
+//  Copyright © 2021 Dmytro Besedin. All rights reserved.
+//
+
+
+public struct Product {
+    let name: String
+    let price: Double
+    public init(name: String, price:Double) {
+      self.name = name
+      self.price = price
+    }
+}
+ open class Purchases {
+  public  var products: [Product] = []
+   open func calculateprice() -> Double {
+        products.reduce(0) { $0 + $1.price }
+    }
+    public init() {}
+}
